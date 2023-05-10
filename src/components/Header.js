78f70d5a8cb682c6e41a707ffb666d54f6fb48af
Ayu-sh-arma <96580931+Ayu-sh-arma@users.noreply.github.com>
2023-05-10 23:49:42 +0530

    header bug squash

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Header = ({handleToggleDarkmode}) => {
     return (
-        <div className='header' onClick={()=>{handleToggleDarkmode((previousDarkMode)=>!previousDarkMode)}}>
+        <div className='header'>
             {/*Getting state using the hook function passed down as prop...know more about this*/}
             <h1>Jotte</h1>
-            <button className='save'>Toggle Modes</button>
+            <button className='save' onClick={()=>{handleToggleDarkmode((previousDarkMode)=>!previousDarkMode)}}>Toggle Modes</button>
         </div>
     )
 }
